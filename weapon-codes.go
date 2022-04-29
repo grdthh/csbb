f614a66618554351fbb9be21247f17f1556a87be
Igor Sudilovskiy <smnsmwr@gmail.com>
2022-04-30 00:51:13 +0500

    initial commit

---
+++ weapon-codes.go
@@ -0,0 +1,8 @@
+package main
+
+var (
+	gear      = []string{"vest", "vesthelm", "defuser", "taser"}
+	grenades  = []string{"molotov", "hegrenade", "flashbang", "smokegrenade", "decoy"}
+	secondary = []string{"hpk2000", "glock", "elite", "p250", "fiveseven", "tec9", "deagle", "revolver"}
+	primary   = []string{"famas", "galilar", "m4a1", "ak47", "ssg08", "aug", "sg556", "awp", "scar20", "g3sg1", "mp9", "mac10", "mp7", "ump45", "p90", "bizon", "nova", "xm1014", "mag7", "sawedoff", "m249", "negev"}
+)
